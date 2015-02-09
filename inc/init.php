4d5d34fa1f25d36be0703b9e8588aef82cc27efc
matteo.cajani <matteo.cajani@gmail.com>
2015-02-09 23:51:56 +0100

    setup of fr_FR translations

--- inc/init.php
+++ inc/init.php
@@ -6,7 +6,7 @@ if (! defined ( 'ABSPATH' )) {
 if ( ! defined( 'YOIMG_COMMONS_PATH' ) ) {
 
 	define ( 'YOIMG_COMMONS_PATH', dirname ( __FILE__ ) );
-	define ( 'YOIMG_SUPPORTED_LOCALES', 'en_US it_IT de_DE nl_NL' ); 
+	define ( 'YOIMG_SUPPORTED_LOCALES', 'en_US it_IT de_DE nl_NL fr_FR' ); 
 	
 	require_once (YOIMG_COMMONS_PATH . '/utils.php');
 	

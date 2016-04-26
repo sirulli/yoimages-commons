e5980098030af97bf8d3982783b2be406359b9d2
Matteo Cajani <matteo@ubuntu-15-04>
2016-04-27 00:19:52 +0200

    moving crop settings for seo module

--- inc/settings.php
+++ inc/settings.php
@@ -7,9 +7,6 @@ if ( ! class_exists( 'YoImagesSettingsPage' ) ) {
 
 	class YoImagesSettingsPage {
 		
-		private $seo_options;
-		private $search_options;
-		
 		public function __construct() {
 			add_action ( 'admin_menu', array ( $this, 'add_plugin_page_menu_item' ) );
 			add_action ( 'admin_init', array ( $this, 'init_admin_page' ) );
@@ -25,8 +22,6 @@ if ( ! class_exists( 'YoImagesSettingsPage' ) ) {
 			}
 			global $yoimg_modules;
 			global $yoimg_plugins_url;
-			$this->seo_options = get_option( 'yoimg_seo_settings' );
-			$this->search_options = get_option( 'yoimg_search_settings' );
 			$settings = apply_filters( 'yoimg_settings', array() );
 			?>
 			<div class="wrap" id="yoimg-settings-wrapper">
@@ -55,7 +50,6 @@ if ( ! class_exists( 'YoImagesSettingsPage' ) ) {
 					<?php
 					}
 					?>
-					<a href="?page=yoimg-settings&tab=yoimages-seo" class="nav-tab <?php echo $active_tab == 'yoimages-seo' ? 'nav-tab-active' : ''; ?>"><?php  _e( 'SEO for images', YOIMG_DOMAIN ); ?></a>
 					<a href="?page=yoimg-settings&tab=yoimages-search" class="nav-tab <?php echo $active_tab == 'yoimages-search' ? 'nav-tab-active' : ''; ?>"><?php  _e( 'Free stock images search', YOIMG_DOMAIN ); ?></a>
 				</h2>
 				<?php
@@ -107,30 +101,13 @@ if ( ! class_exists( 'YoImagesSettingsPage' ) ) {
 				}
 			}
 			
-			register_setting( 'yoimages-seo-group', 'yoimg_seo_settings', array( $this, 'sanitize_seo' ) );
 			register_setting( 'yoimages-search-group', 'yoimg_search_settings', array( $this, 'sanitize_search' ) );
-			
-			add_settings_section( 'yoimg_imgseo_options_section', __( 'SEO for images', YOIMG_DOMAIN ), array( $this, 'print_imgseo_options_section_info' ), 'yoimages-seo' );
-			add_settings_field( 'imgseo_change_image_title', __( 'Change image title', YOIMG_DOMAIN ), array( $this, 'imgseo_change_image_title_callback' ), 'yoimages-seo', 'yoimg_imgseo_options_section' );
-			add_settings_field( 'imgseo_image_title_expression', __( 'Image title expression', YOIMG_DOMAIN), array( $this, 'imgseo_image_title_expression_callback' ), 'yoimages-seo', 'yoimg_imgseo_options_section' );
-			add_settings_field( 'imgseo_change_image_alt', __( 'Change image alt attribute', YOIMG_DOMAIN ), array( $this, 'imgseo_change_image_alt_callback' ), 'yoimages-seo', 'yoimg_imgseo_options_section' );
-			add_settings_field( 'imgseo_image_alt_expression', __( 'Image alt expression', YOIMG_DOMAIN), array( $this, 'imgseo_image_alt_expression_callback' ), 'yoimages-seo', 'yoimg_imgseo_options_section' );
-			add_settings_field( 'imgseo_change_image_filename', __( 'Change image file name', YOIMG_DOMAIN ), array( $this, 'imgseo_change_image_filename_callback' ), 'yoimages-seo', 'yoimg_imgseo_options_section' );
-			add_settings_field( 'imgseo_image_filename_expression', __( 'Image file name expression', YOIMG_DOMAIN), array( $this, 'imgseo_image_filename_expression_callback' ), 'yoimages-seo', 'yoimg_imgseo_options_section' );
 			
 			add_settings_section( 'yoimg_search_options_section', __( 'Free stock images search', YOIMG_DOMAIN ), array( $this, 'print_search_options_section_info' ), 'yoimages-search' );
 			add_settings_field( 'search_is_active', __( 'Enable', YOIMG_DOMAIN ), array( $this, 'search_is_active_callback' ), 'yoimages-search', 'yoimg_search_options_section' );
 			
 		}
-	
-		public function print_imgseo_options_section_info() {
-			print __('Enter your images SEO settings here below', YOIMG_DOMAIN );
-			printf( '<p>' .
-				__( 'Supported expressions:', YOIMG_DOMAIN ) . ' ' . implode( ', ', apply_filters( 'yoimg_supported_expressions', array() ) )
-				. '</p>'
-			);
-		}
-		
+
 		public function print_search_options_section_info() {
 			global $yoimg_search_providers;
 			print __('Free stock images search settings.<br/>Please note that searches are performed in english therefore use english search terms.', YOIMG_DOMAIN );
@@ -146,99 +123,14 @@ if ( ! class_exists( 'YoImagesSettingsPage' ) ) {
 				print '</ul>';
 			}
 		}
-		
-		public function imgseo_change_image_title_callback() {
-			printf(
-				'<input type="checkbox" id="imgseo_change_image_title" name="yoimg_seo_settings[imgseo_change_image_title]" value="TRUE" %s />
-				<p class="description">' . __( 'If checked title will be replaced with the expression here below', YOIMG_DOMAIN ) . '</p>',
-				$this->seo_options['imgseo_change_image_title'] ? 'checked="checked"' : ( YOIMG_DEFAULT_IMGSEO_CHANGE_IMAGE_TITLE && ! isset( $this->seo_options['imgseo_change_image_title'] ) ? 'checked="checked"' : '' )
-			);
-		}
-	
-		public function imgseo_image_title_expression_callback() {
-			printf(
-			'<input type="text" id="imgseo_image_title_expression" name="yoimg_seo_settings[imgseo_image_title_expression]" value="%s" class="imgseo_change_image_title-dep" />
-				<p class="description">' . __( 'expression used to replace the title, accepted values are:', YOIMG_DOMAIN ) . ' ' . implode( ', ', apply_filters( 'yoimg_supported_expressions', array() ) ) . '</p>',
-					! empty( $this->seo_options['imgseo_image_title_expression'] ) ? esc_attr( $this->seo_options['imgseo_image_title_expression'] ) : YOIMG_IMGSEO_IMAGE_TITLE_EXPRESSION
-			);
-		}
-	
-		public function imgseo_change_image_alt_callback() {
-			printf(
-				'<input type="checkbox" id="imgseo_change_image_alt" name="yoimg_seo_settings[imgseo_change_image_alt]" value="TRUE" %s />
-				<p class="description">' . __( 'If checked alt will be replaced with the expression here below', YOIMG_DOMAIN ) . '</p>',
-				$this->seo_options['imgseo_change_image_alt'] ? 'checked="checked"' : ( YOIMG_DEFAULT_IMGSEO_CHANGE_IMAGE_ALT && ! isset( $this->seo_options['imgseo_change_image_alt'] ) ? 'checked="checked"' : '' )
-			);
-		}
-	
-		public function imgseo_image_alt_expression_callback() {
-			printf(
-			'<input type="text" id="imgseo_image_alt_expression" name="yoimg_seo_settings[imgseo_image_alt_expression]" value="%s" class="imgseo_change_image_alt-dep" />
-				<p class="description">' . __( 'expression used to replace the alt, accepted values are:', YOIMG_DOMAIN ) . ' ' . implode( ', ', apply_filters( 'yoimg_supported_expressions', array() ) ) . '</p>',
-					! empty( $this->seo_options['imgseo_image_alt_expression'] ) ? esc_attr( $this->seo_options['imgseo_image_alt_expression'] ) : YOIMG_IMGSEO_IMAGE_ALT_EXPRESSION
-			);
-		}
-	
-		public function imgseo_change_image_filename_callback() {
-			printf(
-				'<input type="checkbox" id="imgseo_change_image_filename" name="yoimg_seo_settings[imgseo_change_image_filename]" value="TRUE" %s />
-				<p class="description">' . __( 'If checked the filename will be replaced with the expression here below', YOIMG_DOMAIN ) . '</p>',
-				$this->seo_options['imgseo_change_image_filename'] ? 'checked="checked"' : ( YOIMG_DEFAULT_IMGSEO_CHANGE_IMAGE_FILENAME && ! isset( $this->seo_options['imgseo_change_image_filename'] ) ? 'checked="checked"' : '' )
-			);
-		}
-	
-		public function imgseo_image_filename_expression_callback() {
-			printf(
-			'<input type="text" id="imgseo_image_filename_expression" name="yoimg_seo_settings[imgseo_image_filename_expression]" value="%s" class="imgseo_change_image_filename-dep" />
-				<p class="description">' . __( 'expression used to replace the filename, accepted values are:', YOIMG_DOMAIN ) . ' ' . implode( ', ', apply_filters( 'yoimg_supported_expressions', array() ) ) . '</p>',
-					! empty( $this->seo_options['imgseo_image_filename_expression'] ) ? esc_attr( $this->seo_options['imgseo_image_filename_expression'] ) : YOIMG_IMGSEO_IMAGE_FILENAME_EXPRESSION
-			);
-		}
 	
 		public function search_is_active_callback() {
+			$search_options = get_option( 'yoimg_search_settings' );
 			printf(
 			'<input type="checkbox" id="search_is_active" name="yoimg_search_settings[search_is_active]" value="TRUE" %s />
 				<p class="description">' . __( 'If checked free stock images search is active', YOIMG_DOMAIN ) . '</p>',
-						$this->search_options['search_is_active'] ? 'checked="checked"' : ( YOIMG_DEFAULT_SEARCH_ENABLED && ! isset( $this->search_options['search_is_active'] ) ? 'checked="checked"' : '' )
+						$search_options['search_is_active'] ? 'checked="checked"' : ( YOIMG_DEFAULT_SEARCH_ENABLED && ! isset( $search_options['search_is_active'] ) ? 'checked="checked"' : '' )
 			);
-		}
-	
-		public function sanitize_seo( $input ) {
-			$new_input = array();
-			if( isset( $input['imgseo_change_image_title'] ) && ( $input['imgseo_change_image_title'] === 'TRUE' || $input['imgseo_change_image_title'] === TRUE ) ) {
-				$new_input['imgseo_change_image_title'] = TRUE;
-			} else {
-				$new_input['imgseo_change_image_title'] = FALSE;
-			}
-			if( isset( $input['imgseo_change_image_alt'] ) && ( $input['imgseo_change_image_alt'] === 'TRUE' || $input['imgseo_change_image_alt'] === TRUE ) ) {
-				$new_input['imgseo_change_image_alt'] = TRUE;
-			} else {
-				$new_input['imgseo_change_image_alt'] = FALSE;
-			}
-			if( isset( $input['imgseo_change_image_filename'] ) && ( $input['imgseo_change_image_filename'] === 'TRUE' || $input['imgseo_change_image_filename'] === TRUE ) ) {
-				$new_input['imgseo_change_image_filename'] = TRUE;
-			} else {
-				$new_input['imgseo_change_image_filename'] = FALSE;
-			}
-			if( isset( $input['imgseo_image_title_expression'] ) && ! empty( $input['imgseo_image_title_expression'] ) ) {
-				$new_input['imgseo_image_title_expression'] = sanitize_text_field( $input['imgseo_image_title_expression'] );
-			} else {
-				add_settings_error( 'yoimg_crop_options_group', 'imgseo_image_title_expression', __( 'title expression is not valid, using default:', YOIMG_DOMAIN ) . ' ' . YOIMG_DEFAULT_IMGSEO_IMAGE_TITLE_EXPRESSION, 'error' );
-				$new_input['imgseo_image_title_expression'] = YOIMG_DEFAULT_IMGSEO_IMAGE_TITLE_EXPRESSION;
-			}
-			if( isset( $input['imgseo_image_alt_expression'] ) && ! empty( $input['imgseo_image_alt_expression'] ) ) {
-				$new_input['imgseo_image_alt_expression'] = sanitize_text_field( $input['imgseo_image_alt_expression'] );
-			} else {
-				add_settings_error( 'yoimg_crop_options_group', 'imgseo_image_alt_expression', __( 'alt expression is not valid, using default:', YOIMG_DOMAIN ) . ' ' . YOIMG_DEFAULT_IMGSEO_IMAGE_ALT_EXPRESSION, 'error' );
-				$new_input['imgseo_image_alt_expression'] = YOIMG_DEFAULT_IMGSEO_IMAGE_ALT_EXPRESSION;
-			}
-			if( isset( $input['imgseo_image_filename_expression'] ) && ! empty( $input['imgseo_image_filename_expression'] ) ) {
-				$new_input['imgseo_image_filename_expression'] = sanitize_text_field( $input['imgseo_image_filename_expression'] );
-			} else {
-				add_settings_error( 'yoimg_crop_options_group', 'imgseo_image_filename_expression', __( 'filename expression is not valid, using default:', YOIMG_DOMAIN ) . ' ' . YOIMG_DEFAULT_IMGSEO_IMAGE_FILENAME_EXPRESSION, 'error' );
-				$new_input['imgseo_image_filename_expression'] = YOIMG_DEFAULT_IMGSEO_IMAGE_FILENAME_EXPRESSION;
-			}
-			return $new_input;
 		}
 	
 		public function sanitize_search( $input ) {
